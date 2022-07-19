1e2c9144ff2c10ee91ebd2697e4cf4546ea75f33
Talha <106121597+WebDevTalha@users.noreply.github.com>
2022-07-19 15:21:21 -0700

    admin setup done with login

---
+++ config.php
@@ -0,0 +1,44 @@
+<?php
+$host = "localhost";
+$db_name = "psms";
+$user = "root";
+$password = "";
+
+date_default_timezone_set("Asia/Dhaka");
+try {
+  $pdo = new PDO("mysql:host=$host;dbname=$db_name", $user, $password);
+  $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+} catch(PDOException $e) {
+  echo "Connection failed: " . $e->getMessage();
+}
+
+
+
+// Count any Column Value from Profile Table
+// function pfRowCount($col, $val) {
+//   global $pdo;
+//   $stm=$pdo->prepare("SELECT $col FROM students WHERE $col=?");
+//   $stm->execute(array($val));
+//   $count = $stm->rowCount();
+//   return $count;
+// }
+
+// GET Student Data
+// function student($col,$id){
+//   global $pdo;
+//   $stm = $pdo->prepare("SELECT $col FROM students WHERE id=?");
+//   $stm->execute(array($id));
+//   $result = $stm->fetchAll(PDO::FETCH_ASSOC);
+//   return $result[0][$col];
+// }
+
+
+// GET Admin Data
+function admin($col,$id){
+  global $pdo;
+  $stm = $pdo->prepare("SELECT $col FROM admin WHERE id=?");
+  $stm->execute(array($id));
+  $result = $stm->fetchAll(PDO::FETCH_ASSOC);
+  return $result[0][$col];
+}
